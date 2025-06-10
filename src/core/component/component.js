89682b3c301191b63b5aaeae56c3f6cd69ffabf3
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-06-10 20:43:25 +1000

    Implement base Component class for inheritance

---
+++ src/core/component/component.js
@@ -0,0 +1,13 @@
+export class Component {
+	constructor(props = {}) {
+		this.props = props;
+	}
+
+	/**
+	 * Render the component content.
+	 * @returns {HTMLElement}
+	 */
+	render() {
+		throw new Error('Render method must be implemented in the child class');
+	}
+}
