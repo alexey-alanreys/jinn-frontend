c30deb2328501e917f8b254e2bb171a44adf368d
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-06-11 20:34:35 +1000

    Implement chart configuration

---
+++ src/config/chart.config.js
@@ -0,0 +1,104 @@
+const daysOfWeek = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+const months = [
+	'Янв',
+	'Фев',
+	'Мар',
+	'Апр',
+	'Май',
+	'Июн',
+	'Июл',
+	'Авг',
+	'Сен',
+	'Окт',
+	'Ноя',
+	'Дек',
+];
+
+const chartOptions = {
+	autoSize: true,
+	layout: {
+		background: { type: 'solid', color: '#ffffff' },
+		textColor: '#191919',
+		attributionLogo: false,
+	},
+	rightPriceScale: {
+		scaleMargins: { top: 0.05, bottom: 0.05 },
+		borderVisible: false,
+	},
+	crosshair: {
+		mode: 0,
+		vertLine: { color: '#a5a5a5', style: 3 },
+		horzLine: { color: '#a5a5a5', style: 3 },
+	},
+	grid: {
+		vertLines: { color: '#edf0ee' },
+		horzLines: { color: '#edf0ee' },
+	},
+	localization: {
+		timeFormatter: (time) => {
+			const date = new Date(time * 1000);
+			const dayOfWeek = daysOfWeek[date.getUTCDay()];
+			const day = date.getUTCDate();
+			const month = months[date.getUTCMonth()];
+			const year = date.getUTCFullYear();
+			const hours = String(date.getUTCHours()).padStart(2, '0');
+			const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+			return `${dayOfWeek} ${day} ${month} ${year}   ${hours}:${minutes}`;
+		},
+	},
+	timeScale: {
+		rightOffset: 5,
+		barSpacing: 8,
+		minBarSpacing: 2,
+		borderVisible: false,
+		rightBarStaysOnScroll: true,
+
+		tickMarkFormatter: (time) => {
+			const date = new Date(time * 1000);
+			const day = date.getUTCDate();
+			const month = date.getUTCMonth();
+			const year = date.getUTCFullYear();
+			const hours = date.getUTCHours();
+			const minutes = date.getUTCMinutes();
+
+			if (month === 0 && day === 1 && hours === 0 && minutes === 0) {
+				return String(year);
+			}
+
+			if (day === 1 && hours === 0 && minutes === 0) {
+				return months[month];
+			}
+
+			if (hours === 0 && minutes === 0) {
+				return String(day);
+			}
+
+			const formattedHours = String(hours).padStart(2, '0');
+			const formattedMinutes = String(minutes).padStart(2, '0');
+			return `${formattedHours}:${formattedMinutes}`;
+		},
+	},
+};
+
+const candlestickOptions = {
+	upColor: '#008984',
+	downColor: '#f23645',
+	borderVisible: false,
+	wickUpColor: '#008984',
+	wickDownColor: '#f23645',
+};
+
+const indicatorOptions = {
+	lineWidth: 2,
+	lastValueVisible: false,
+	priceLineVisible: false,
+	crosshairMarkerVisible: false,
+};
+
+export {
+	chartOptions,
+	candlestickOptions,
+	indicatorOptions,
+	daysOfWeek,
+	months,
+};
