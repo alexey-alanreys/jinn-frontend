e31ab9111e188cc862f739f7922a568ba6f35bff
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-07-25 15:43:29 +1000

    Update README and add virtualization for TradesTab component

--- src/components/layout/dashboard/report/report-content/trades-tab/trades-tab.component.js
+++ src/components/layout/dashboard/report/report-content/trades-tab/trades-tab.component.js
@@ -1,15 +1,15 @@
-import { reportService } from '@/api/services/report.service';
 import { BaseComponent } from '@/core/component/base.component';
 import { $Q } from '@/core/libs/query.lib';
 import { renderService } from '@/core/services/render.service';
 import { stateService } from '@/core/services/state.service';
 
+import { reportService } from '@/api/services/report.service';
 
-import { ToggleSortingButton } from './toggle-sorting-button/toggle-sorting-button.component';
-import { TradesItem } from './trades-item/trades-item.component';
 import styles from './trades-tab.module.css';
 import templateHTML from './trades-tab.template.html?raw';
 
+import { ToggleSortingButton } from './toggle-sorting-button/toggle-sorting-button.component';
+import { TradesItem } from './trades-item/trades-item.component';
 
 export class TradesTab extends BaseComponent {
 	static COMPONENT_NAME = 'TradesTab';
@@ -17,7 +17,14 @@ export class TradesTab extends BaseComponent {
 	#$element;
 
 	#cachedTrades = [];
-	#items = new Map();
+	#renderedItems = new Map();
+	#visibleStartIndex = 0;
+	#visibleEndIndex = 0;
+	#itemHeight = 100;
+	#minVisibleItems = 10;
+	#scrollTop = 0;
+	#lastScrollTime = 0;
+	#scrollThrottleDelay = 30;
 
 	render() {
 		this.#initComponents();
@@ -32,7 +39,9 @@ export class TradesTab extends BaseComponent {
 			const trades = await reportService.getTrades(context.id);
 			this.#cachedTrades = [...trades];
 
+			this.#clearRenderedItems();
 			this.#ensureCorrectSortOrder();
+			this.#calculateVisibleRange();
 			this.#renderTrades();
 		} catch (error) {
 			console.error('Failed to update trades.', error);
@@ -41,7 +50,9 @@ export class TradesTab extends BaseComponent {
 
 	handleSortingToggle() {
 		if (this.#cachedTrades.length > 0) {
+			this.#clearRenderedItems();
 			this.#applySorting();
+			this.#calculateVisibleRange();
 			this.#renderTrades();
 		}
 	}
@@ -70,8 +81,24 @@ export class TradesTab extends BaseComponent {
 	}
 
 	#setupInitialState() {
-		stateService.subscribe('context', this.update.bind(this));
+		this.#attachListeners();
 		this.update(stateService.get('context'));
+	}
+
+	#attachListeners() {
+		this.#$element.on('scroll', this.#handleScroll.bind(this));
+		stateService.subscribe('context', this.update.bind(this));
+	}
+
+	#handleScroll() {
+		const now = Date.now();
+		if (now - this.#lastScrollTime < this.#scrollThrottleDelay) return;
+
+		this.#lastScrollTime = now;
+		this.#scrollTop = this.#$element.element.scrollTop;
+
+		this.#calculateVisibleRange();
+		this.#renderTrades();
 	}
 
 	#ensureCorrectSortOrder() {
@@ -86,35 +113,68 @@ export class TradesTab extends BaseComponent {
 		}
 	}
 
+	#calculateVisibleRange() {
+		const minItemsToShow = Math.max(
+			this.#minVisibleItems,
+			Math.ceil(this.#$element.element.clientHeight / this.#itemHeight) + 2,
+		);
+
+		this.#visibleStartIndex = Math.floor(this.#scrollTop / this.#itemHeight);
+		this.#visibleEndIndex = Math.min(
+			this.#visibleStartIndex + minItemsToShow - 1,
+			this.#cachedTrades.length - 1,
+		);
+	}
+
 	#renderTrades() {
 		const $items = this.#$element.find('[data-ref="tradesItems"]');
-		this.#removeOrphanedItems();
+		const totalHeight = this.#cachedTrades.length * this.#itemHeight;
+		$items.css('height', `${totalHeight}px`);
 
-		this.#cachedTrades.forEach((trade, index) => {
-			let item = this.#items.get(index);
+		const newRenderedItems = new Map();
 
-			if (!item) {
+		for (let i = this.#visibleStartIndex; i <= this.#visibleEndIndex; i++) {
+			const trade = this.#cachedTrades[i];
+			let item;
+
+			if (this.#renderedItems.has(i)) {
+				item = this.#renderedItems.get(i);
+			} else {
 				item = new TradesItem();
-				this.#items.set(index, item);
 				$items.append(item.render());
+
+				const $item = $Q(item.element);
+				$item.css({
+					position: 'absolute',
+					top: `${i * this.#itemHeight}px`,
+					left: '0',
+					right: '0',
+					height: `${this.#itemHeight}px`,
+				});
+
+				item.update(trade);
 			}
 
-			item.update(trade);
-		});
+			newRenderedItems.set(i, item);
+		}
+
+		for (const [i, item] of this.#renderedItems) {
+			if (!newRenderedItems.has(i)) {
+				item.element.remove();
+			}
+		}
+
+		this.#renderedItems = newRenderedItems;
+	}
+
+	#clearRenderedItems() {
+		for (const item of this.#renderedItems.values()) {
+			item.element.remove();
+		}
+		this.#renderedItems.clear();
 	}
 
 	#applySorting() {
 		this.#cachedTrades.reverse();
-	}
-
-	#removeOrphanedItems() {
-		const validKeys = new Set(this.#cachedTrades.map((_, i) => i));
-
-		this.#items.forEach((item, key) => {
-			if (!validKeys.has(key)) {
-				item.remove();
-				this.#items.delete(key);
-			}
-		});
 	}
 }
